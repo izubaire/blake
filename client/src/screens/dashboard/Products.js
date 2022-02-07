d195e57a93ab716fcef69b1f4ab75534dfda2ca1
izubaire <m.zobaire@gmail.com>
2022-02-07 21:58:10 +0500

    Make Dashboard Content section

--- client/src/screens/dashboard/Products.js
+++ client/src/screens/dashboard/Products.js
@@ -1,9 +1,9 @@
-import Sidebar from "../../components/Sidebar";
+import Wrapper from "./Wrapper"
 const Products = () => {
     return(
-        <>
-        <Sidebar />
-        </>
+       <Wrapper>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel molestiae tempora voluptatibus rem neque optio, deserunt, autem, est distinctio assumenda ratione cum esse at. Vero inventore officia perspiciatis quisquam consequatur!
+       </Wrapper>
     )
 }
 export default Products;
