07978f9e97a321889593e8feef836168abe7a318
izubaire <m.zobaire@gmail.com>
2022-08-06 18:04:38 +0500

    Create search bar and implement search functionality

--- client/src/screens/home/SearchProducts.js
+++ client/src/screens/home/SearchProducts.js
@@ -1,22 +1,22 @@
 import { useParams } from "react-router-dom";
 import Header from "../../components/home/Header";
 import Nav from "../../components/home/Nav";
-import { useCatProductsQuery } from "../../store/services/homeProducts";
+import { useSearchProductsQuery } from "../../store/services/homeProducts";
 import ProductCard from "../../components/home/ProductCard";
 import Pagination from "../../components/Pagination";
 import ProductSkeleton from "../../components/home/ProductSkeleton";
 
-const CatProducts = () => {
-  const { name, page = 1 } = useParams();
-  const { data, isFetching } = useCatProductsQuery({
-    name,
+const SearchProducts = () => {
+  const { keyword, page = 1 } = useParams();
+  const { data, isFetching } = useSearchProductsQuery({
+    keyword,
     page: parseInt(page),
   });
   return (
     <>
       <Nav />
       <div className="mt-[70px]">
-        <Header>#{name}</Header>
+        <Header>#{keyword}</Header>
       </div>
       <div className="my-container my-10">
         {isFetching ? (
@@ -24,7 +24,7 @@ const CatProducts = () => {
         ) : data.count > 0 ? (
           <>
             <p className="text-base font-medium text-gray-700">
-              {data.count} products found in #{name} category
+              {data.count} products found for #{keyword} keyword
             </p>
             <div className="flex flex-wrap -mx-5">
               {data.products.map((product) => {
@@ -35,16 +35,18 @@ const CatProducts = () => {
               page={parseInt(page)}
               perPage={data.perPage}
               count={data.count}
-              path={`cat-products/${name}`}
+              path={`cat-products/${keyword}`}
               theme="light"
             />
           </>
         ) : (
-          <p className="alert-danger">No products found in #{name} category</p>
+          <p className="alert-danger">
+            No products found for #{keyword} keyword
+          </p>
         )}
       </div>
     </>
   );
 };
 
-export default CatProducts;
+export default SearchProducts;
