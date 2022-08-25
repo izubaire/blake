42d7e971e74b6a0f5619f451dd629fe0907c2f92
izubaire <m.zobaire@gmail.com>
2022-08-25 21:51:37 +0500

    Stripe dynamic products and webhook

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -1,25 +1,40 @@
-const { Router } = require("express");
-const stripe = require("stripe")(process.env.STRIPE_KEY);
-const router = Router();
-router.post("/create-checkout-session", async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "T-shirt",
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    success_url: `${process.env.CLIENT}`,
-    cancel_url: `${process.env.CLIENT}/cart`,
-  });
+const express = require("express");
+const stripe = require("stripe");
+const PaymentController = require("../controllers/PaymentController");
+const router = express.Router();
+router.post("/create-checkout-session", PaymentController.paymentProcess);
+router.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
+    const sig = request.headers["stripe-signature"];
 
-  res.json({ url: session.url });
-});
+    let event;
+    try {
+      event = stripe.webhooks.constructEvent(
+        request.rawBody,
+        sig,
+        process.env.ENDPOINTSECRET
+      );
+    } catch (err) {
+      console.log(err.message);
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntent = event.data.object;
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
+);
 module.exports = router;
